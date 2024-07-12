47a14f9b3e41d9280cd85090e75f04e0eddae0b9
sarajit <bdsarajit499@gamil.com>
2024-07-12 09:11:34 +0600

    sign up and login modal complete

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ var openModalBtnTwo = document.getElementById("openModalBtnTwo");
 
 // Get close button
 var closeBtn = document.getElementsByClassName("close")[0];
+// Get close button
+var closeBtnOne = document.getElementsByClassName("close")[1];
 
 // Get modal sign in element
 var modalSignIn = document.getElementById("mySignInModal");
@@ -57,13 +59,29 @@ openModalBtnSignIn.onclick = ()=>{
 closeBtn.onclick = function() {
     modal.classList.add("hidden");
 }
+// Listen for close click
+closeBtnOne.onclick = function() {
+    modalSignIn.classList.add("hidden");
+}
 
 
 
 // Listen for outside click
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modal || event.target == modalSignIn) {
         modal.classList.add("hidden");
+        modalSignIn.classList.add("hidden");
     }
 }
+
+const postBtn = document.querySelector('.post_button')
+//scroll 
+window.onscroll = ((e)=>{
+ console.log(window.scrollY)
+  const screenHeight = screen.height - 170
+   postBtn.style.top = `${screenHeight + window.scrollY}px`
+ console.log(screenHeight , screen.height)
+
+
+})
 
