26d2fef2760146664c444c874967f85fe5ac45f7
sarajit <bdsarajit499@gamil.com>
2024-07-12 07:19:44 +0600

    destop design complete

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const dropDownBtn = document.querySelectorAll(".drop_down_btn");
 const dropDownMenu = document.querySelectorAll(".drop_down");
+// Get modal element
+var modal = document.getElementById("myModal");
+
+// Get open modal button
+var openModalBtn = document.getElementById("openModalBtn");
+// Get open modal button
+var openModalBtnTwo = document.getElementById("openModalBtnTwo");
+
+// Get close button
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Get modal sign in element
+var modalSignIn = document.getElementById("mySignInModal");
+
+// Get open modal sign in button
+var openModalBtnSignIn = document.getElementById("signInOpenModalBtn");
+
+
+
+
 
 dropDownBtn.forEach((btn, btnInx) => {
   btn.addEventListener("click", () => {
@@ -15,24 +35,30 @@ dropDownBtn.forEach((btn, btnInx) => {
 
 
 
-// Get modal element
-var modal = document.getElementById("myModal");
-
-// Get open modal button
-var openModalBtn = document.getElementById("openModalBtn");
-
-// Get close button
-var closeBtn = document.getElementsByClassName("close")[0];
 
 // Listen for open click
 openModalBtn.onclick = function() {
+  modalSignIn.classList.add("hidden")
     modal.classList.remove("hidden");
 }
+// Listen for open tWO click
+openModalBtnTwo.onclick = function() {
+  modalSignIn.classList.add("hidden")
+    modal.classList.remove("hidden");
+}
+openModalBtnSignIn.onclick = ()=>{
+  modal.classList.add("hidden");
+  modalSignIn.classList.remove("hidden")
+}
+
+
 
 // Listen for close click
 closeBtn.onclick = function() {
     modal.classList.add("hidden");
 }
+
+
 
 // Listen for outside click
 window.onclick = function(event) {
